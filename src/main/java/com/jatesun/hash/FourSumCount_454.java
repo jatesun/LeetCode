8c2fdf05a8a17bfb2fa8573288dbe4d222c08683
jatesun <jatesun111@gmail.com>
2024-05-11 09:59:50 +0800

    2024年05月05日 leetcode454：四数相加

---
+++ src/main/java/com/jatesun/hash/FourSumCount_454.java
@@ -0,0 +1,45 @@
+package com.jatesun.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
+ * 0 <= i, j, k, l < n
+ * nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+ * 示例 1：
+ * 输入：nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+ * 输出：2
+ * 解释：
+ * 两个元组如下：
+ * 1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1) + 2 = 0
+ * 2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
+ * 示例 2：
+ * 输入：nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+ * 输出：
+ * 提示：
+ * n == nums1.length
+ * n == nums2.length
+ * n == nums3.length
+ * n == nums4.length
+ * 1 <= n <= 200
+ * -228 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 228
+ */
+public class FourSumCount_454 {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int res = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i : nums1) {
+            for (int j : nums2) {
+                int sum = i + j;
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+        for (int i : nums3) {
+            for (int j : nums4) {
+                res += map.getOrDefault(0 - i - j, 0);
+            }
+        }
+        return res;
+    }
+}
