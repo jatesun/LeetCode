23d05733f57e4f5ece78aad467b337a316080294
jatesun <jatesun111@gmail.com>
2024-05-20 11:10:14 +0800

    2024年05月05日 leetcode20：判断括号

---
+++ src/main/java/com/jatesun/stack/IsValid_20.java
@@ -0,0 +1,50 @@
+package com.jatesun.stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 每个右括号都有一个对应的相同类型的左括号。
+ * 示例 1：
+ * 输入：s = "()"
+ * 输出：true
+ * 示例 2：
+ * 输入：s = "()[]{}"
+ * 输出：true
+ * 示例 3：
+ * 输入：s = "(]"
+ * 输出：false
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 104
+ * s 仅由括号 '()[]{}' 组成
+ */
+public class IsValid_20 {
+
+    public boolean isValid(String s) {
+        Deque<Character> deque = new LinkedList<>();
+        char ch;
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+            //碰到左括号，就把相应的右括号入栈
+            if (ch == '(') {
+                deque.push(')');
+            } else if (ch == '{') {
+                deque.push('}');
+            } else if (ch == '[') {
+                deque.push(']');
+            } else if (deque.isEmpty() || deque.peek() != ch) {
+                return false;
+            } else {//如果是右括号判断是否和栈顶元素匹配
+                deque.pop();
+            }
+        }
+        //最后判断栈中元素是否匹配
+        return deque.isEmpty();
+    }
+
+}
