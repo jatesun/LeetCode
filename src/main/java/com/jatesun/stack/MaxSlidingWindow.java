d06e3c9ff5c89181c4bc893acb8010bdfe2e98c8
jatesun <jatesun111@gmail.com>
2024-05-23 14:32:10 +0800

    2024年05月05日 leetcode239：滑动窗口的最大值。第二种实现

---
+++ src/main/java/com/jatesun/stack/MaxSlidingWindow.java
@@ -0,0 +1,29 @@
+package com.jatesun.stack;
+
+public class MaxSlidingWindow {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 1) {
+            return nums;
+        }
+        int len = nums.length - k + 1;
+        //存放结果元素的数组
+        int[] res = new int[len];
+        int num = 0;
+        //自定义队列
+        MyQueue myQueue = new MyQueue();
+        //先将前k的元素放入队列
+        for (int i = 0; i < k; i++) {
+            myQueue.add(nums[i]);
+        }
+        res[num++] = myQueue.peek();
+        for (int i = k; i < nums.length; i++) {
+            //滑动窗口移除最前面的元素，移除是判断该元素是否放入队列
+            myQueue.poll(nums[i - k]);
+            //滑动窗口加入最后面的元素
+            myQueue.add(nums[i]);
+            //记录对应的最大值
+            res[num++] = myQueue.peek();
+        }
+        return res;
+    }
+}
