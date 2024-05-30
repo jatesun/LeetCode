262887781929a3e91fd91eda021b815bfba907bd
jatesun <jatesun111@gmail.com>
2024-05-30 17:39:56 +0800

    2024年05月05日 leetcode145：树的后序遍历。

---
+++ src/main/java/com/jatesun/tree/PostorderTraversal_145.java
@@ -0,0 +1,35 @@
+package com.jatesun.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给你一棵二叉树的根节点 root ，返回其节点值的 后序遍历 。
+ * 示例 1：
+ * 输入：root = [1,null,2,3]
+ * 输出：[3,2,1]
+ * 示例 2：
+ * 输入：root = []
+ * 输出：[]
+ * 示例 3：
+ * 输入：root = [1]
+ * 输出：[1]
+ * 提示：
+ * 树中节点的数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ * 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+ */
+public class PostorderTraversal_145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        innerOrder(root, result);
+        return result;
+    }
+
+    public void innerOrder(TreeNode cur, List<Integer> result) {
+        if (cur == null) return;
+        innerOrder(cur.left, result);
+        innerOrder(cur.right, result);
+        result.add(cur.val);
+    }
+}
