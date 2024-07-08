58145d37305f587fb0fa2d106e87424dc7825a39
jatesun <jatesun111@gmail.com>
2024-07-08 14:25:04 +0800

    2024年05月05日 leetcode617：合并树。（队列实现）

--- src/main/java/com/jatesun/tree/MergeTrees_617.java
+++ src/main/java/com/jatesun/tree/MergeTrees_617.java
@@ -1,5 +1,8 @@
 package com.jatesun.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 给你两棵二叉树： root1 和 root2 。
  * 想象一下，当你将其中一棵覆盖到另一棵之上时，两棵树上的一些节点将会重叠（而另一些不会）。你需要将这两棵树合并成一棵新二叉树。合并的规则是：如果两个节点重叠，那么将这两个节点的值相加作为合并后节点的新值；否则，不为 null 的节点将直接作为新二叉树的节点。
@@ -28,6 +31,39 @@ public class MergeTrees_617 {
         root1.val += root2.val;
         root1.left = mergeTrees(root1.left, root2.left);
         root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
+
+    public TreeNode mergeTrees1(TreeNode root1, TreeNode root2) {
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root1);
+        queue.offer(root2);
+        while (!queue.isEmpty()) {
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+            // 此时两个节点一定不为空，val相加
+            node1.val = node1.val + node2.val;
+            // 如果两棵树左节点都不为空，加入队列
+            if (node1.left != null && node2.left != null) {
+                queue.offer(node1.left);
+                queue.offer(node2.left);
+            }
+            // 如果两棵树右节点都不为空，加入队列
+            if (node1.right != null && node2.right != null) {
+                queue.offer(node1.right);
+                queue.offer(node2.right);
+            }
+            // 若node1的左节点为空，直接赋值
+            if (node1.left == null && node2.left != null) {
+                node1.left = node2.left;
+            }
+            // 若node1的右节点为空，直接赋值
+            if (node1.right == null && node2.right != null) {
+                node1.right = node2.right;
+            }
+        }
         return root1;
     }
 }
