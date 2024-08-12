4ac76355c155b0611cd56bc56342e0c2410e6fe4
jatesun <jatesun111@gmail.com>
2024-08-12 16:52:14 +0800

    2024年08月12日 77. 组合

---
+++ src/main/java/com/jatesun/backtracking/Combine_77.java
@@ -0,0 +1,49 @@
+package com.jatesun.backtracking;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+ * 你可以按 任何顺序 返回答案。
+ * 示例 1：
+ * 输入：n = 4, k = 2
+ * 输出：
+ * [
+ * [2,4],
+ * [3,4],
+ * [2,3],
+ * [1,2],
+ * [1,3],
+ * [1,4],
+ * ]
+ * 示例 2：
+ * <p>
+ * 输入：n = 1, k = 1
+ * 输出：[[1]]
+ * 提示：
+ * 1 <= n <= 20
+ * 1 <= k <= n
+ */
+public class Combine_77 {
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        backtracking(n, k, 1);
+        return result;
+    }
+
+    public void backtracking(int n, int k, int startIndex) {
+        if (path.size() == k) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = startIndex; i <= n; i++) {
+            path.add(i);
+            backtracking(n, k, i + 1);
+            path.removeLast();
+        }
+    }
+}
